42a9400a195c68ad7e513286bec7e6d5f5da822e
Harsh28Pandey <harsh28.knp@gmail.com>
2026-04-18 17:55:48 +0530

    Update apiPaths.js

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5173";
 
 //* utils/apiPaths.js
 export const API_PATHS = {
